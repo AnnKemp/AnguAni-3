21ae4a7e34332629ec8bcb2b11d0238ea58836bb
AnnKemp <ann.kemp@scarlet.be>
2021-02-03 17:03:12 +0100

    added component and some css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RedComponent } from './red/red.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RedComponent
   ],
   imports: [
     BrowserModule,
