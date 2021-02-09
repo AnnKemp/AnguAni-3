216575007172182f206726887509e6af75782882
AnnKemp <ann.kemp@scarlet.be>
2021-02-09 21:33:51 +0100

    no flying yet . . .

--- src/app/fly-in-out/fly-in-out.component.ts
+++ src/app/fly-in-out/fly-in-out.component.ts
@@ -4,7 +4,20 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
 @Component({
   selector: 'app-fly-in-out',
   templateUrl: './fly-in-out.component.html',
-  styleUrls: ['./fly-in-out.component.css']
+  styleUrls: ['./fly-in-out.component.css'],
+  animations: [
+    trigger('flyInOut', [
+      state('in', style({ transform: 'translateX(0)'})),
+      transition('void => *', [
+        style({ transform: 'translateX(-100%)' }),
+        animate(100)
+      ]),
+      transition('* => void', [
+        style({ transform: 'translateX(100%)' }),
+        animate(100)
+    ]),
+   ])
+  ]
 })
 export class FlyInOutComponent {
 
