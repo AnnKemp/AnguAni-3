18c8d780821689a38f3b0988c85ef87e974abdc2
AnnKemp <ann.kemp@scarlet.be>
2021-02-22 23:09:17 +0100

    removed errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+ 
+
 
 import { AppComponent } from './app.component';
 import { RedComponent } from './red/red.component';
@@ -33,8 +34,8 @@ import { ReactivePersonaldataFormComponent } from './reactive-personaldata-form/
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
-    ReactiveFormsModule,
-    HttpModule
+    ReactiveFormsModule
+ 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -8,13 +8,21 @@ import { NgForm } from '@angular/forms'; /* om de form te */
 })
 export class FormComponent{
 
-secrets=['Your first Pet?', 'Your favorite teacher?']; // hier zit nog iets mis mee :veranderen! moet er this voor of array of . . .
+public secrets=['Your first Pet?', 'Your favorite teacher?']; // hier zit nog iets mis mee :veranderen! moet er this voor of array of . . .
  
 // ergens moeten ook de gegevens van de form als een soort array of zo worden opgeslagen om door te sturen naar de server
   submitted = false; 
 
-  onSubmit(){
+  private _submitMessage=''; // een lege var te maken
+
+get submitMessage(){
+  return this._submitMessage; // om de waarden van het formulier door te sturen via get-functie
+}
+  
+  onSubmit(form: NgForm){
     this.submitted = true; // om de form te verstoppen
+    this._submitMessage=JSON.stringify(form.value); // om de waarden van het formulier te tonen
+    
 
     // this is for testing if the form works:
     //console.log('Submitted!'); // to test if it works! Calling the onSubmit method from the form by clicking the submit button (F12)
