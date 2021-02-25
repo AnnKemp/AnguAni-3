c76afd9c85c5dbe0bd69772cbc6a8db76f73d2a5
AnnKemp <ann.kemp@scarlet.be>
2021-02-25 23:24:39 +0100

    nuilding on the .ts file

--- src/app/hero-form/hero-form.component.ts
+++ src/app/hero-form/hero-form.component.ts
@@ -8,9 +8,17 @@ import { Hero } from '../hero';
 })
 export class HeroFormComponent {
 
-skills=['Angular', 'React', 'PHP', 'C#'];
+skills = ['Angular', 'React', 'PHP', 'C#'];
 
-model=new Hero(52, 'unemployed, this.skills[0], 'Front-end Developer');
+model = new Hero(52, 'unemployed, this.skills[0], 'Front-end Developer');
+
+// the following code creates a new hero instance, so that the initial form can show an example hero
+const myHero = new Hero( 42, 'couchHero',
+'CSS-specialist',
+'Old-school-Webdesigner'
+);
+console.log('My hero is called ' + myHero.name); 
+
 
 submitted = false; // wanneer form niet verstuurd
  
